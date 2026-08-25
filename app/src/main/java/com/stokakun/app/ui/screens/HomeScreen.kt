7fb04a26b9a5f34a953c4c31758442b4ef5cfa02
woolcynder-zee <woolcynder@gmail.com>
2026-08-25 12:40:00 +0700

    fix: opt in to experimental Material3 APIs on home screen

--- app/src/main/java/com/stokakun/app/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/stokakun/app/ui/screens/HomeScreen.kt
@@ -25,6 +25,7 @@ import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExtendedFloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -56,6 +57,7 @@ import com.stokakun.app.util.BackupManager
 import com.stokakun.app.viewmodel.AccountViewModel
 import kotlinx.coroutines.launch
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeScreen(
     viewModel: AccountViewModel,
