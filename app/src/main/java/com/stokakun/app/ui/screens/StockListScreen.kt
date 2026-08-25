2b30c0ee98e27b2ee4d447f263a472d7171f40de
woolcynder-zee <woolcynder@gmail.com>
2026-08-25 16:00:59 +0700

    fix: correct horizontalScroll import

--- app/src/main/java/com/stokakun/app/ui/screens/StockListScreen.kt
+++ app/src/main/java/com/stokakun/app/ui/screens/StockListScreen.kt
@@ -1,11 +1,11 @@
 package com.stokakun.app.ui.screens
 
+import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.horizontalScroll
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.lazy.LazyColumn
