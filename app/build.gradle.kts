05963a3eb3207b91f86869a6727f9b7826f85073
woolcynder-zee <woolcynder@gmail.com>
2026-08-25 10:20:04 +0700

    Remove unused security crypto dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,7 +46,6 @@ dependencies {
     implementation("androidx.room:room-runtime:2.6.1")
     implementation("androidx.room:room-ktx:2.6.1")
     ksp("androidx.room:room-compiler:2.6.1")
-    implementation("androidx.security:security-crypto:1.1.0-alpha06")
     implementation("io.coil-kt:coil-compose:2.6.0")
     debugImplementation("androidx.compose.ui:ui-tooling")
 }
